9e6e784fe97a940f39fcc0527a24371b81b1527a
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-14 21:58:08 +0300

    a sample test

--- src/test/java/testCode/sample.py
+++ src/test/java/testCode/sample.py
@@ -1,5 +1,10 @@
 import pytest
 
+test_data = [
+    (1, 1)
+]
 
-def test_sample():
-    assert 1 == 1
+
+@pytest.mark.parametrize("input,expected_result", test_data)
+def test_sample(input: int, expected_result: int):
+    assert expected_result == input
