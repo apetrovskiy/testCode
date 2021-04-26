f8065cbffc802698ee98ac36d81023e32c27dd5a
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-04-26 03:57:57 +0300

    one more task

--- src/main/java/testCode/challenges/SumOfSquareOfFirstNOddNumbers/sum_of_square_of_first_n_odd_numbers.py
+++ src/main/java/testCode/challenges/SumOfSquareOfFirstNOddNumbers/sum_of_square_of_first_n_odd_numbers.py
@@ -1,2 +1,2 @@
 def sum_of_square_of_first_n_odd_numbers(input: int) -> int:
-    return 0
+    return sum([x**2 for x in range(1, input*2+1, 2) if x % 2 == 1])
--- src/test/java/testCode/challenges/SumOfSquareOfFirstNOddNumbers/test_sum_of_square_of_first_n_odd_numbers.py
+++ src/test/java/testCode/challenges/SumOfSquareOfFirstNOddNumbers/test_sum_of_square_of_first_n_odd_numbers.py
@@ -6,11 +6,14 @@ from src.main.java.testCode.challenges.SumOfSquareOfFirstNOddNumbers. \
 
 
 test_data = [
-    (3,35)
+    (3, 35),
+    (5, 165),
+    (1, 1)
 ]
 
 
 @pytest.mark.parametrize("input_number,expected_result", test_data)
 def test_sum_of_square_of_first_n_odd_numbers(
-        input_number:int, expected_result: int):
-    assert expected_result == sum_of_square_of_first_n_odd_numbers(input_number)
+        input_number: int, expected_result: int):
+    assert expected_result == sum_of_square_of_first_n_odd_numbers(
+        input_number)
