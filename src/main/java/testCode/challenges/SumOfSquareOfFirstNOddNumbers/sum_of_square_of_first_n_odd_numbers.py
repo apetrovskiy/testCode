6d97fe8098936df652418b8921b4673f52346093
Aleksandr Petrovskiy <uiautomation@hotmail.com>
2023-09-18 21:58:03 +0300

    fixes && formatting

--- src/main/java/testCode/challenges/SumOfSquareOfFirstNOddNumbers/sum_of_square_of_first_n_odd_numbers.py
+++ src/main/java/testCode/challenges/SumOfSquareOfFirstNOddNumbers/sum_of_square_of_first_n_odd_numbers.py
@@ -1,2 +1,2 @@
 def sum_of_square_of_first_n_odd_numbers(input: int) -> int:
-    return sum([x**2 for x in range(1, input*2+1, 2) if x % 2 == 1])
+    return sum([x**2 for x in range(1, input * 2 + 1, 2) if x % 2 == 1])
